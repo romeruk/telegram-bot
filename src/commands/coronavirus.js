dc72966f709d5e4a0f8e66983f767fb07cfdad41
Andrey 1 <romeruk21@gmail.com>
2020-02-18 21:40:19 +0200

    trycatch add

--- src/commands/coronavirus.js
+++ src/commands/coronavirus.js
@@ -2,11 +2,16 @@ import axios from 'axios';
 
 const coronaVirus = (bot) => {
   bot.command("coronavirus", async (ctx) => {
-    const getInfo = await axios.get("https://2019ncov.asia/api/cdr");
-    const [{ confirmed }, { deaths }, { recovered }] = getInfo.data[1].results;
-    return ctx.reply(`Всього заразилось: <b>${confirmed}</b>\nВсього померло: <b>${deaths}</b>\nВсього вилікувалось: <b>${recovered}</b>`, {
-      parse_mode: "HTML"
-    });
+    try {
+      const getInfo = await axios.get("https://2019ncov.asia/api/cdr");
+      const [{ confirmed }, { deaths }, { recovered }] = getInfo.data[1].results;
+      return ctx.reply(`Всього заразилось: <b>${confirmed}</b>\nВсього померло: <b>${deaths}</b>\nВсього вилікувалось: <b>${recovered}</b>`, {
+        parse_mode: "HTML"
+      });
+    } catch (error) {
+      console.log(error);
+      return ctx.reply("error");
+    }
   });
 };
 
--- src/inlineQueries/herostats.js
+++ src/inlineQueries/herostats.js
@@ -9,26 +9,30 @@ const herostats = (bot) => {
 
     //no sense to do api reguest if page > 3, becose there 130 heroes avaible in dota 2
     if (page < 4) {
+      try {
 
-      const heroes_stats = await axios.get(`${process.env.OPEN_DOTA_BASE_API_URL}/heroStats?api_key=${process.env.OPEN_DOTA_KEY}`);
+        const heroes_stats = await axios.get(`${process.env.OPEN_DOTA_BASE_API_URL}/heroStats?api_key=${process.env.OPEN_DOTA_KEY}`);
 
-      const data = heroes_stats.data.slice((page - 1) * dataCount, page * dataCount);
+        const data = heroes_stats.data.slice((page - 1) * dataCount, page * dataCount);
 
 
-      let result = data.map((hero, i) => {
-        return {
-          type: "article",
-          id: String(i),
-          title: hero.localized_name,
-          input_message_content: {
-            message_text: `<b>Назва героя:</b> ${hero.localized_name}\n<b>Основний атрибут:</b> ${primary_attr(hero.primary_attr)}\n<b>Базова швидкість руху:</b> ${hero.move_speed}\n<b>Тип атаки:</b> ${attack_type(hero.attack_type)}\n<b>Базова сила:</b> ${hero.base_str}\n<b>Базова спритність:</b> ${hero.base_agi}\n<b>Базовий інтелект:</b> ${hero.base_int}\n<b>Іграбельні позиції:</b> ${hero.roles.join()}`,
-            parse_mode: "HTML"
-          },
-          thumb_url: `https://api.opendota.com${hero.icon}`
-        }
-      });
+        let result = data.map((hero, i) => {
+          return {
+            type: "article",
+            id: String(i),
+            title: hero.localized_name,
+            input_message_content: {
+              message_text: `<b>Назва героя:</b> ${hero.localized_name}\n<b>Основний атрибут:</b> ${primary_attr(hero.primary_attr)}\n<b>Базова швидкість руху:</b> ${hero.move_speed}\n<b>Тип атаки:</b> ${attack_type(hero.attack_type)}\n<b>Базова сила:</b> ${hero.base_str}\n<b>Базова спритність:</b> ${hero.base_agi}\n<b>Базовий інтелект:</b> ${hero.base_int}\n<b>Іграбельні позиції:</b> ${hero.roles.join()}`,
+              parse_mode: "HTML"
+            },
+            thumb_url: `https://api.opendota.com${hero.icon}`
+          }
+        });
 
-      return ctx.answerInlineQuery(result);
+        return ctx.answerInlineQuery(result);
+      } catch (error) {
+        console.log(error);
+      }
     }
 
   });
