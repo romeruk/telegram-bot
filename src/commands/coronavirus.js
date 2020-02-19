3e47dba9f6d50c7ae14840ab506f12b5cb32f863
Andrey 1 <romeruk21@gmail.com>
2020-02-19 11:43:32 +0200

    timeout /coronavirus command

--- src/commands/coronavirus.js
+++ src/commands/coronavirus.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 const coronaVirus = (bot) => {
   bot.command("coronavirus", async (ctx) => {
     try {
-      const getInfo = await axios.get("https://2019ncov.asia/api/cdr");
+      const getInfo = await axios.get("https://2019ncov.asia/api/cdr", {
+        timeout: 20 * 1000
+      });
       const [{ confirmed }, { deaths }, { recovered }] = getInfo.data[1].results;
       return ctx.reply(`Всього заразилось: <b>${confirmed}</b>\nВсього померло: <b>${deaths}</b>\nВсього вилікувалось: <b>${recovered}</b>`, {
         parse_mode: "HTML"
