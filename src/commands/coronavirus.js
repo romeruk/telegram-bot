6ab5b96c198fa77d30ed66a3510bfb22acb90e07
Andrey 1 <romeruk21@gmail.com>
2020-02-19 13:10:43 +0200

    minor fix

--- src/commands/coronavirus.js
+++ src/commands/coronavirus.js
@@ -6,11 +6,12 @@ const coronaVirus = (bot) => {
       const response = await axios.get("https://www.worldometers.info/coronavirus/");
       const $ = cheerio.load(response.data);
       const [confirmed = 0, deaths = 0, recovered = 0] = Array.from($('.maincounter-number'));
+      const data = $("#page-top").next().text();
       // const getInfo = await axios.get("https://2019ncov.asia/api/cdr", {
       //   timeout: 20 * 1000
       // });
       // const [{ confirmed }, { deaths }, { recovered }] = getInfo.data[1].results;
-      return ctx.reply(`Всього заразилось: <b>${$(confirmed).text()}</b>\nВсього померло: <b>${$(deaths).text()}</b>\nВсього вилікувалось: <b>${$(recovered).text()}</b>`, {
+      return ctx.reply(`${data}\nВсього заразилось: <b>${$(confirmed).text()}</b>\nВсього померло: <b>${$(deaths).text()}</b>\nВсього вилікувалось: <b>${$(recovered).text()}</b>`, {
         parse_mode: "HTML"
       });
     } catch (error) {
