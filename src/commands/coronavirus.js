3aa4aa302287401275bbf186e1763c48dcc1f705
Andrey 1 <romeruk21@gmail.com>
2020-02-19 12:58:01 +0200

    cherio testing

--- src/commands/coronavirus.js
+++ src/commands/coronavirus.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
-
+import cheerio from "cheerio";
 const coronaVirus = (bot) => {
   bot.command("coronavirus", async (ctx) => {
     try {
-      const getInfo = await axios.get("https://2019ncov.asia/api/cdr", {
-        timeout: 20 * 1000
-      });
-      const [{ confirmed }, { deaths }, { recovered }] = getInfo.data[1].results;
-      return ctx.reply(`Всього заразилось: <b>${confirmed}</b>\nВсього померло: <b>${deaths}</b>\nВсього вилікувалось: <b>${recovered}</b>`, {
+      const response = await axios.get("https://www.worldometers.info/coronavirus/");
+      const $ = cheerio.load(response.data);
+      const [confirmed = 0, deaths = 0, recovered = 0] = Array.from($('.maincounter-number'));
+      // const getInfo = await axios.get("https://2019ncov.asia/api/cdr", {
+      //   timeout: 20 * 1000
+      // });
+      // const [{ confirmed }, { deaths }, { recovered }] = getInfo.data[1].results;
+      return ctx.reply(`Всього заразилось: <b>${$(confirmed).text()}</b>\nВсього померло: <b>${$(deaths).text()}</b>\nВсього вилікувалось: <b>${$(recovered).text()}</b>`, {
         parse_mode: "HTML"
       });
     } catch (error) {
       console.log(error);
+      return;
     }
   });
 };
--- src/commands/index.js
+++ src/commands/index.js
@@ -1,9 +1,11 @@
 import randomPosition from './randomPosition';
 import randomQuestion from './randomQuestion';
 import coronaVirus from './coronavirus';
+import coronaVirus2 from './coronavirus2';
 
 export default (bot) => {
   randomPosition(bot);
   randomQuestion(bot);
   coronaVirus(bot);
+  coronaVirus2(bot);
 }
