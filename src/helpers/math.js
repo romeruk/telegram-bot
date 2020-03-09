5bb0f49a1cd0c1c2a7295937c1cad8aa4c462307
Andrey 1 <romeruk21@gmail.com>
2020-03-09 18:21:40 +0200

    fix date minutes format

--- src/helpers/math.js
+++ src/helpers/math.js
@@ -11,16 +11,17 @@ export const formatDate = (date) => {
   let day = '' + d.getDate();
   let year = d.getFullYear();
   let hours = d.getHours();
-  let minutes = d.getMinutes();
+  let minutes = '' + d.getMinutes();
   let seconds = '' + d.getSeconds();
 
   if (month.length < 2)
     month = '0' + month;
   if (day.length < 2)
     day = '0' + day;
-  if (seconds.length < 2) {
+  if (minutes.length < 2)
+    minutes = '0' + minutes;
+  if (seconds.length < 2)
     seconds = '0' + seconds;
-  }
 
   return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 }
